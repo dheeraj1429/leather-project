44f11359697b1e4c258a740bfa8202d0cd2c98d3
dheeraj1429 <dheerajsingh1429@gmail.com>
2022-06-30 14:36:00 +0530

    leather project

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+// // dom notes
+// const sliderDots = document.querySelectorAll(".dot");
+
+// sliderDots.forEach((el) =>
+//     el.addEventListener("click", function () {
+//         sliderDots.forEach((elm) => {
+//             elm.classList.remove("active-dot");
+//         });
+//         this.classList.add("active-dot");
+//     })
+// );
